962d4c14408b28d2944eeaaa1cc219f278451d2d
AmpF5 <lolc44t@gmail.com>
2023-08-22 19:21:46 +0200

    first commit

---
+++ src/FullstackWiki.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace FullstackWiki.Infrastructure {
+    public static class DependencyInjection {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
