b114f7641008a8c22d6869d2130678f1c5321f96
AmpF5 <lolc44t@gmail.com>
2023-09-21 18:07:11 +0200

    progress

--- src/FullstackWiki.Presentation/ClientApp/src/app/app-routing.module.ts
+++ src/FullstackWiki.Presentation/ClientApp/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RoutingComponent } from './navbar/front-end/routing/routing.component';
 
 let routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'frontend/routing/details/:id', component: RoutingComponent},
+  { path: 'frontend/routing', component: RoutingComponent, children:[
+    {path: 'firstChild', component: RoutingComponent},
+    {path: 'secondChild', component: RoutingComponent}
+  ]}
 ];
 
 
--- src/FullstackWiki.Presentation/ClientApp/src/app/navbar/front-end/routing/routing.component.ts
+++ src/FullstackWiki.Presentation/ClientApp/src/app/navbar/front-end/routing/routing.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import hljs from 'highlight.js';
 
 @Component({
@@ -9,6 +10,22 @@ import hljs from 'highlight.js';
 export class RoutingComponent implements AfterViewInit{
   ngAfterViewInit(): void {
     hljs.highlightAll();
-    // hljs.initHighlightingOnLoad();
   }
+  
+  constructor(private router: Router, private route: ActivatedRoute){
+    
+  }
+
+  protected navigate(id: number) {
+    this.router.navigate(['frontend/routing/details/', id])
+  }
+
+  protected navigateToFirstChild() {
+    this.router.navigate(['firstChild'], {relativeTo: this.route});
+  }
+
+  protected navigateToSecondChild() {
+    this.router.navigate(['secondChild'], {relativeTo: this.route});
+  }
+
 }
